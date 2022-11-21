813b3d8784dcb4bf7701f6d67332a86c7228d0be
malanak2 <malanakgames@seznam.cz>
2022-11-21 17:55:49 +0100

    Fixed the message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 from unidecode import unidecode
 load_dotenv()
-## On this line replace this with "@app.route('/your_url', methods=['GET', 'POST'])" or the function wont work
+## On this line replace this with "TARGET_URL = 'your_server:your_port/your_after_slash_in_the_other_file_same'" or the function wont work
 
 KEY = os.getenv('KEY')
 fernet = Fernet(KEY)
